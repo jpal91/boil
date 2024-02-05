927eaa009461c3bc3e92488f337d46ac948ccf17
jpal91 <pallanschja@gmail.com>
2024-02-05 16:43:55 -0500

    added new macro colorize

--- src/utils.rs
+++ src/utils.rs
@@ -11,15 +11,49 @@ macro_rules! capitalize {
     };
 }
 
+pub fn color_str(input: &str, tag: &str) -> String {
+    let mut it = tag.chars().peekable();
+    let mut attr: Vec<&str> = vec![];
+    let mut newline = "";
+
+    while let Some(m) = it.next() {
+        match m {
+            'F' => {
+                if let Some(n) = it.peek() {
+                    let col = match n {
+                        'r' => "31",
+                        'g' => "32",
+                        'y' => "33",
+                        'b' => "34",
+                        'm' => "35",
+                        'c' => "36",
+                        'w' => "37",
+                        _ => ""
+                    };
+                    if !col.is_empty() {
+                        it.next();
+                        attr.push(col)
+                    }
+                }
+            },
+            'b' => attr.push("1"),
+            'i' => attr.push("3"),
+            'u' => attr.push("4"),
+            'N' => newline = "\n",
+            _ => {}
+        }
+    };
+
+    format!("{}\x1b[{}m{}\x1b[0m", newline, attr.join(";"), input)
+}
+
 macro_rules! print_color {
     () => (print!("\n"));
 
     ( $tag:ident -> $msg:expr, $($rest:tt)* ) => {
         {
-            match stringify!($tag) {
-                "f" => print!("\x1b[35m{}\x1b[0m ", $msg),
-                _ => print!("{} ", $msg)
-            };
+            let color = color_str($msg, stringify!($tag));
+            print!("{} ", color);
             print_color!($($rest)*);
         }
     };
@@ -27,12 +61,36 @@ macro_rules! print_color {
     ( $tag:ident -> $msg:expr ) => (print_color!($tag -> $msg,));
 
 
-    ( $msg:expr, $($rest:tt)* ) => (print_color!(N->$msg, $($rest)*));
+    ( $msg:expr, $($rest:tt)* ) => (print_color!(X->$msg, $($rest)*));
+
+    ( $msg:expr ) => (print_color!(X->$msg,));
         
 }
 
+macro_rules! colorize {
 
-pub(crate) use {capitalize, print_color};
+    () => {String::new()};
+
+    ( [ $($acc:tt)* ]; $tag:ident -> $msg:expr, $($rest:tt)* ) => {
+        {
+            let color = color_str($msg, stringify!($tag));
+            colorize!([ $($acc)* color, ]; $($rest)* )
+        }
+    };
+
+    ( [ $($acc:tt)* ]; $msg:expr, $($rest:tt)* ) => {colorize!([$($acc)*]; X->$msg, $($rest)*)};
+
+    ( [ $($acc:tt)* ]; $tag:ident -> $msg:expr ) => {colorize!([$($acc)*]; $tag -> $msg,)};
+
+    ( [ $($acc:tt)* ]; $msg:expr ) => {colorize!([$($acc)*]; X->$msg,)};
+
+    ( [ $($acc:tt)* ];) =>  { [$($acc)*].join(" ") };
+
+    ( $($any:tt)* ) => { colorize!([]; $($any)* ) };
+}
+
+
+pub(crate) use {capitalize, print_color, colorize};
 
 #[cfg(test)]
 mod tests {
@@ -40,7 +98,14 @@ mod tests {
 
     #[test]
     fn test_color() {
-        print_color!(f->"testing", "testing", f->"testing2", "testing4", "testing5", f->"testing6");
+        print_color!(Fr->"testing", Fbbi->"testing1", b->"testing2", x->"testing3", "testing4", Fgbu->"testing5");
+        print_color!("hello");
+    }
+
+    #[test]
+    fn test_colorize() {
+        let col = colorize!(Fgb->"hello again", N->"hello", Fr->"goodbye", "again" );
+        println!("{}", col)
     }
 
 }
